26c2091eb30efc3a068f2b7ae66f465b75b741f6
ristovljupcho <ristov.ljupcho@gmail.com>
2025-05-28 20:08:03 +0200

    Removing attributes that are not needed.

--- src/main/java/com/weatherapp/entities/Forecast.java
+++ src/main/java/com/weatherapp/entities/Forecast.java
@@ -2,6 +2,8 @@ package com.weatherapp.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -12,7 +14,6 @@ import lombok.Setter;
 import lombok.ToString;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
@@ -22,54 +23,16 @@ import java.util.UUID;
 @NoArgsConstructor
 @Entity
 public class Forecast {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     private LocalDate forecastDate;
 
-    private LocalDateTime sunrise;
-
-    private LocalDateTime sunset;
-
     private double tempDay;
 
-    private double tempMin;
-
-    private double tempMax;
-
-    private Double tempNight;
-
-    private Double tempEve;
-
-    private Double tempMorn;
-
-    private Double feelsLikeDay;
-
-    private Double feelsLikeNight;
-
-    private Double feelsLikeEve;
-
-    private Double feelsLikeMorn;
-
-    private Integer pressure;
-
-    private Integer humidity;
-
     private String weatherMain;
-
-    private String weatherDescription;
-
-    private Double windSpeed;
-
-    private Integer windDeg;
-
-    private Double windGust;
-
-    private Integer clouds;
-
-    private Double pop;
-
-    private Double rain;
 
     @ToString.Exclude
     @ManyToOne(fetch = FetchType.LAZY)
