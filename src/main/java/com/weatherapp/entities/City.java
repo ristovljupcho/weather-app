32c25a98bed43c8ea36cb1951120b54b3726c900
ristovljupcho <ristov.ljupcho@gmail.com>
2025-05-28 20:07:37 +0200

    Fixing bugs in City.

--- src/main/java/com/weatherapp/entities/City.java
+++ src/main/java/com/weatherapp/entities/City.java
@@ -1,8 +1,9 @@
 package com.weatherapp.entities;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
@@ -22,7 +23,7 @@ import java.util.UUID;
 @Entity
 public class City {
     @Id
-    @Column(columnDefinition = "CHAR(36)")
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     private String name;
