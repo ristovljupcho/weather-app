074c740021a298657e4d6d10e2ecd828d4ab9637
ristovljupcho <ristov.ljupcho@gmail.com>
2025-05-29 11:20:15 +0200

    Added new methods in ForecastRepository.

--- src/main/java/com/weatherapp/repositories/ForecastRepository.java
+++ src/main/java/com/weatherapp/repositories/ForecastRepository.java
@@ -2,8 +2,15 @@ package com.weatherapp.repositories;
 
 import com.weatherapp.entities.Forecast;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ForecastRepository extends JpaRepository<Forecast, UUID> {
+    @Query("SELECT f FROM Forecast f WHERE f.tempDay >= 25")
+    List<Forecast> findWarmDays();
+
+    @Query("SELECT f FROM Forecast f WHERE f.weatherMain IN ('Rain', 'Drizzle', 'Thunderstorm')")
+    List<Forecast> findRainyDays();
 }
